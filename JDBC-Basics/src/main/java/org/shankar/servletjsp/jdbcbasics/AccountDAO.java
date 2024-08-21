495ed25832daa983846ec40988dae09cfea2a450
Shankar Chakraborty <40225075+Shankar022@users.noreply.github.com>
2024-08-21 21:53:14 +0530

    optimised the code and added logback.xml

--- JDBC-Basics/src/main/java/org/shankar/servletjsp/jdbcbasics/AccountDAO.java
+++ JDBC-Basics/src/main/java/org/shankar/servletjsp/jdbcbasics/AccountDAO.java
@@ -1,25 +1,64 @@
 package org.shankar.servletjsp.jdbcbasics;
 
-import java.sql.*;
+import com.zaxxer.hikari.HikariConfig;
+import com.zaxxer.hikari.HikariDataSource;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+import java.sql.Connection;
+import java.sql.PreparedStatement;
+import java.sql.ResultSet;
+import java.sql.SQLException;
 
 
 public class AccountDAO {
-    public static void main(String[] args) {
-        try (
-                Connection connection = DriverManager.getConnection("jdbc:mysql://localhost/mydb", "root", "root12345");
-                Statement statement = connection.createStatement();
-                ResultSet resultSet = statement.executeQuery("select * from account");
-        ) {
+    private static final Logger logger = LoggerFactory.getLogger(AccountDAO.class);
 
+//    public static void main(String[] args) {
+//        try (
+//                Connection connection = DriverManager.getConnection("jdbc:mysql://localhost/mydb", "root", "root12345");
+//                Statement statement = connection.createStatement();
+//                ResultSet resultSet = statement.executeQuery("select * from account");
+//        ) {
+//
+//            while (resultSet.next()) {
+//                logger.info("Full name : {} {}, Balance : {}", resultSet.getString(2), resultSet.getString(3), resultSet.getInt(4));
+//            }
+//        } catch (SQLException e) {
+//            logger.error(e.getLocalizedMessage());
+//        }
+//    }
+
+    private static final HikariDataSource dataSource;
+
+    static {
+        HikariConfig config = new HikariConfig();
+        config.setJdbcUrl("jdbc:mysql://localhost/mydb");
+        config.setUsername("root");
+        config.setPassword("root12345");
+        config.setConnectionTimeout(30000);
+        config.setIdleTimeout(60000);
+        config.setMaximumPoolSize(10);
+        dataSource = new HikariDataSource(config);
+    }
+
+    public static void main(String[] args) {
+        String query = "SELECT accno, firstname, lastname, bal FROM account";
+
+        try (
+                Connection connection = dataSource.getConnection();
+                PreparedStatement preparedStatement = connection.prepareStatement(query);
+                ResultSet resultSet = preparedStatement.executeQuery();
+        ) {
             while (resultSet.next()) {
-                System.out.println(resultSet.getString(2));
-                System.out.println(resultSet.getString(3));
-                System.out.println(resultSet.getInt(4));
+                String firstName = resultSet.getString(2);  // first_name
+                String lastName = resultSet.getString(3);  // last_name
+                int balance = resultSet.getInt(4);
+                logger.info("Full name : {} {}, Balance : {}", firstName, lastName, balance);// balance
             }
         } catch (SQLException e) {
-            e.printStackTrace();
+            logger.error(e.getLocalizedMessage());
         }
-
-        // TODO : Use PreparedStatement and  Hikari Pool to poll the db connection
     }
 }
+

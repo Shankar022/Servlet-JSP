684883c8d38e10053132ae2c294f21c7a4c558cb
Shankar Chakraborty <40225075+Shankar022@users.noreply.github.com>
2024-08-21 13:00:50 +0530

    retrieve all data from account table using ResultSet

--- JDBC-Basics/src/main/java/org/shankar/servletjsp/jdbcbasics/AccountDAO.java
+++ JDBC-Basics/src/main/java/org/shankar/servletjsp/jdbcbasics/AccountDAO.java
@@ -2,42 +2,23 @@ package org.shankar.servletjsp.jdbcbasics;
 
 import java.sql.*;
 
+
 public class AccountDAO {
     public static void main(String[] args) {
         try {
             Connection connection = DriverManager.getConnection("jdbc:mysql://localhost/mydb", "root", "root12345");
             System.out.println(connection);
             Statement statement = connection.createStatement();
-//            int executed = statement.executeUpdate("insert into account values (123, 'Shankar', 'Chakraborty', 10000)");
-//            System.out.println(executed + " rows got affected");
-
-            int executed = statement.executeUpdate("update account set bal=50000 where accno=123");
-            System.out.println(executed + " rows got affected");
+            ResultSet resultSet = statement.executeQuery("select * from account");
+            while (resultSet.next()){
+                System.out.println(resultSet.getString(2));
+                System.out.println(resultSet.getString(3));
+                System.out.println(resultSet.getInt(4));
+            }
         } catch (SQLException e) {
             e.printStackTrace();
         }
 
-//        String url = "jdbc:mysql://localhost/mydb";
-//        String username = "root";
-//        String password = "root12345";
-//
-//        String insertQuery = "INSERT INTO account (id, first_name, last_name, balance) VALUES (?, ?, ?, ?)";
-//
-//        try (Connection connection = DriverManager.getConnection(url, username, password);
-//             PreparedStatement preparedStatement = connection.prepareStatement(insertQuery)) {
-//
-//            // Set parameters for the PreparedStatement
-//            preparedStatement.setInt(1, 123);
-//            preparedStatement.setString(2, "Shankar");
-//            preparedStatement.setString(3, "Chakraborty");
-//            preparedStatement.setDouble(4, 10000.00);
-//
-//            // Execute the query
-//            int rowsAffected = preparedStatement.executeUpdate();
-//            System.out.println(rowsAffected + " rows got affected");
-//
-//        } catch (SQLException e) {
-//            e.printStackTrace();
-//        }
+        // TODO : Use PreparedStatement and  Hikari Pool to poll the db connection
     }
 }

9c0e57bf637a258f2db4a65d9e035ec415603ca3
Shankar Chakraborty <40225075+Shankar022@users.noreply.github.com>
2024-08-21 13:03:11 +0530

    used try-with-resource to auto close / cleanup the JDBC resources

--- JDBC-Basics/src/main/java/org/shankar/servletjsp/jdbcbasics/AccountDAO.java
+++ JDBC-Basics/src/main/java/org/shankar/servletjsp/jdbcbasics/AccountDAO.java
@@ -5,12 +5,13 @@ import java.sql.*;
 
 public class AccountDAO {
     public static void main(String[] args) {
-        try {
-            Connection connection = DriverManager.getConnection("jdbc:mysql://localhost/mydb", "root", "root12345");
-            System.out.println(connection);
-            Statement statement = connection.createStatement();
-            ResultSet resultSet = statement.executeQuery("select * from account");
-            while (resultSet.next()){
+        try (
+                Connection connection = DriverManager.getConnection("jdbc:mysql://localhost/mydb", "root", "root12345");
+                Statement statement = connection.createStatement();
+                ResultSet resultSet = statement.executeQuery("select * from account");
+        ) {
+
+            while (resultSet.next()) {
                 System.out.println(resultSet.getString(2));
                 System.out.println(resultSet.getString(3));
                 System.out.println(resultSet.getInt(4));
